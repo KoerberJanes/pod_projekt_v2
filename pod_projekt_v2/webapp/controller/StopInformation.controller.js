30b22b47133828e375868ef552f34e46a6785a82
Aries961 <jnannt@googlemail.com>
2024-06-04 17:52:40 +0200

    Navigation bei StopInformation funktioniert

--- pod_projekt_v2/webapp/controller/StopInformation.controller.js
+++ pod_projekt_v2/webapp/controller/StopInformation.controller.js
@@ -17,6 +17,42 @@ sap.ui.define([
                 
             },
 
+            onPressBtnAvisNr: function(oEvent){
+                /*
+                const oSingleStopModel = this.getView().getModel("SingleStopModel").getProperty("/results");
+                const {phoneAvis} = oSingleStopModel;
+                sap.m.URLHelper.triggerTel(phoneAvis);//phoneAvis
+                const telefonBtn = this.getView().byId("btnAvisNr");
+                const toQuittierungsBtn = this.getView().byId("QuittierungBtn");
+                telefonBtn.setType("Default");
+                toQuittierungsBtn.setType("Emphasized");
+                */
+            },
+
+            onNavToMap:function(){
+                var oRouter = this.getOwnerComponent().getRouter();
+        
+                oRouter.navTo("MapView");
+            },
+
+            onNavToAbladung:function(){
+                var oRouter = this.getOwnerComponent().getRouter();
+        
+                oRouter.navTo("Abladung");
+            },
+
+            onNavToQuittierung:function(){
+                var oRouter = this.getOwnerComponent().getRouter();
+        
+                oRouter.navTo("Quittierung");
+            },
+
+            onNavToActiveTour:function(){
+                var oRouter = this.getOwnerComponent().getRouter();
+        
+                oRouter.navTo("ActiveTour");
+            },
+
             onRefreshRespectiveStops:function(){
                 
                 MessageToast.show("Dies ist ein Dummy-Rrefresh!", {
